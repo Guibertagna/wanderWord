d1a9d32ef15dcd0872f74be99fc30dda8179aed0
Guibertagna <guirogeriobertagna00@gmail.com>
2024-03-19 20:46:58 -0300

    Refatoração de codigo para MVC

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,46 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { FirebaseService } from 'src/app/model/service/firebase-service.service';
+  import { Component, OnInit } from '@angular/core';
+  import { FirebaseService } from 'src/app/model/service/firebase-service.service';
+  import { Observable } from 'rxjs';
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
-})
-export class HomePage implements OnInit {
-  pdf!: File; // Adicionando o operador ! para indicar que a propriedade será inicializada
+  @Component({
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
+  })
+  export class HomePage implements OnInit {
+    file!: File; 
+    percentage!: Observable<number>; 
 
-  constructor(private firebaseService: FirebaseService) { }
+    constructor(private firebaseService: FirebaseService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
-  // Método para lidar com o upload do arquivo PDF
-  handleUpload(event: any) {
-    const file = event.target.files[0]; // Obtém o arquivo PDF do evento de upload
-    if (file) {
-      this.pdf = file; // Armazena o arquivo PDF na propriedade pdf
+    handleUpload(event: any) {
+      const file = event.target.files[0]; 
+      if (file) {
+        this.file = file; 
+      }
     }
-  }
 
-  // Método para chamar a função de upload do serviço FirebaseService
-  uploadPDF() {
-    if (this.pdf) {
-      this.firebaseService.uploadAndSaveEbook(this.pdf, {})
-        .subscribe(
-          (url) => {
-            if (url) {
-              console.log('URL do arquivo PDF:', url);
-              // Faça algo com a URL do arquivo PDF, se necessário
-            } else {
-              console.error('Erro ao fazer upload do arquivo PDF');
-            }
-          },
-          (error) => {
-            console.error('Erro ao fazer upload do arquivo PDF:', error);
-          }
-        );
-    } else {
-      console.error('Nenhum arquivo PDF selecionado');
+    uploadfile() {
+      if (this.file) {
+        this.percentage = this.firebaseService.uploadfile(this.file);
+      } else {
+        console.error('Nenhum arquivo file selecionado');
+      }
     }
+    
   }
-}
